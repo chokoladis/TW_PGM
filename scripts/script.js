e5b4a0801f18945bfc226043339172aed29dcc12
chokoladis <igor2011__igor@mail.ru>
2021-10-28 14:14:26 +0400

    first commit, 2 hours

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+      $('.slider').slick({
+            dots:true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+      //   responsive: [
+      //     {
+      //       breakpoint: 768,
+      //       settings: {
+      //         slidesToShow: 2,
+      //         slidesToScroll: 2
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 560,
+      //       settings: {
+      //         slidesToShow: 1,
+      //         slidesToScroll: 1
+      //       }
+      //     }
+      //   ]
+      });
+    
+});
