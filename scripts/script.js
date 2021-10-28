a4925d40451887080b82dc5e1a282430290a4d5c
chokoladis <igor2011__igor@mail.ru>
2021-10-28 20:04:57 +0400

    5hours in work

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,35 @@ $(document).ready(function(){
             dots:true,
             slidesToShow: 3,
             slidesToScroll: 1,
-      //   responsive: [
-      //     {
-      //       breakpoint: 768,
-      //       settings: {
-      //         slidesToShow: 2,
-      //         slidesToScroll: 2
-      //       }
-      //     },
-      //     {
-      //       breakpoint: 560,
-      //       settings: {
-      //         slidesToShow: 1,
-      //         slidesToScroll: 1
-      //       }
-      //     }
-      //   ]
-      });
+            responsive: [
+                  {
+                        breakpoint: 930,
+                        settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                        }
+                  },
+                  {
+                        breakpoint: 700,
+                        settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                        }
+                  }
+            ]
+            });
+            // let n = $(".slider_item").length();
+            // let array = [];
+            // let four,bool;
+            // if($(window).innerWidth<930){
+            //       for (let i=0;i<=n;i++){
+            //             four++;
+            //             arr[i] = $(".slider_item").html();
+            //             //element.remove()
+            //             if (four==4){
+
+            //             }
+            //       }
+            // }
     
 });
